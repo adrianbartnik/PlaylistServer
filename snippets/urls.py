eb89309f320add76f96d26b68bfcc88cb6eb29d6
Adrian Bartnik <adrian.bartnik@web.de>
2015-05-06 15:31:07 +0200

    Chapter 4 from tutorial now complete.

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,6 +5,8 @@ from snippets import views
 urlpatterns = (
     url(r'^api/$', views.SnippetList.as_view()),
     url(r'^api/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^api/users/$', views.UserList.as_view()),
+    url(r'^api/users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
