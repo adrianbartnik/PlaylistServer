ded626eb51a96e95b92f437fad8629a6efead717
Adrian Bartnik <adrian.bartnik@web.de>
2015-05-06 22:46:41 +0200

    Finished REST Tutorial

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,14 +1,11 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
-urlpatterns = (
-    url(r'^snippet/$', views.SnippetList.as_view(), name="snippet-list"),
-    url(r'^snippet/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name="snippet-detail"),
-    url(r'^snippet/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name="snippet-highlight"),
-    url(r'^users/$', views.UserList.as_view(), name="users-list"),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name="users-detail"),
-    url(r'^$', views.api_root),
-)
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = (
+    url(r'^', include(router.urls)),
+)
